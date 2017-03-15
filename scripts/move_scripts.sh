7d936fa9f8ab54c8e232cbc1c735bdb90aebbd1f
Rasmus Tavi <Rasmus.Tavi@pc.fmi.fi>
2017-03-15 16:29:39 +0200

    integrate gitflow

--- scripts/move_scripts.sh
+++ scripts/move_scripts.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/bash
 
-mkdir -p cmake && cp ../modules/utility/make_msvc.sh cmake/make_msvc.sh
+mkdir -p git
+cp -r ../modules/gitflow/* ./git/
+rm  ./git/LICENSE
+rm ./git/README.md
+
+mkdir -p cmake && cp ../modules/util/make_msvc.sh cmake/make_msvc.sh
+
+read -rsp $'Press any key to continue...\n' -n 1 key
